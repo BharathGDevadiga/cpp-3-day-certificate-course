7c51dadfb91c96b824c7a7e914221d58479071fb
bharath <myasuslaptop45@gmail.com>
2026-07-31 12:59:12 +0530

    Create count_even_odd.cpp

---
+++ Day_02/count_even_odd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = 6;
+
+    int evenCount = 0;
+    int oddCount = 0;
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            evenCount++;
+        }
+        else{
+            oddCount++;
+        }
+    }
+
+    cout << "Even numbers: " << evenCount << endl;
+    cout << "Odd numbers: " << oddCount;
+    return 0;
+}
