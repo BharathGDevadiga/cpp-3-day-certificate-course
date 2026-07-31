4047da50a510fca697419788d6e7f384eeb98683
bharath <myasuslaptop45@gmail.com>
2026-07-31 22:01:30 +0530

    Create tower_of_hanoi.cpp

---
+++ Day_02/tower_of_hanoi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char source, char helper, char destination)
+{
+	if(n == 0)
+  {
+		return;
+	}
+
+	towerOfHanoi(n - 1, source, destination, helper);
+	cout << "Move disk " << n << " from " << source<< " to " << destination << endl;
+	towerOfHanoi(n - 1, helper, source, destination);
+}
+
+int main()
+{
+	towerOfHanoi(3, 'A', 'B', 'C');
+	return 0;
+}
