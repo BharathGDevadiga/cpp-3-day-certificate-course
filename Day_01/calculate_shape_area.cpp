d3cd88b09d762ebcc1555b52aa4d2c4677f553ab
bharath <myasuslaptop45@gmail.com>
2026-07-31 12:49:27 +0530

    Create  calculate_shape_area.cpp

---
+++ Day_01/calculate_shape_area.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Choose (1=Circle, 2=Rectangle, 3=Triangle): ";
+    int choice;
+    cin >> choice;
+
+    switch (choice)
+      {
+        case 1: 
+          {
+            double r; cout << "Enter radius: "; cin >> r;
+            cout << "Area = " << 3.14159 * r * r << endl;
+            break;
+        }
+        case 2: 
+          {
+            double l, w; cout << "Enter length and width: "; cin >> l >> w;
+            cout << "Area = " << l * w << endl;
+            break;
+        }
+        case 3:
+          {
+            double b, h; cout << "Enter base and height: "; cin >> b >> h;
+            cout << "Area = " << 0.5 * b * h << endl;
+            break;
+        }
+        default:
+            cout << "Invalid choice." << endl;
+    }
+
+    return 0;
+}
