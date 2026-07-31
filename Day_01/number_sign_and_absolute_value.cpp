c395d659157177c477263180c49e99aaef54a4e1
bharath <myasuslaptop45@gmail.com>
2026-07-31 12:47:33 +0530

    Create number_sign_and_absolute_value.cpp

---
+++ Day_01/number_sign_and_absolute_value.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int number;
+
+   cout << "Enter a number: ";
+   cin >> number;
+
+   if (number < 0) 
+   {
+      cout << "The number is negative." << endl;
+      cout << "Absolute value: " << -number << endl;
+   }
+   else if (number > 0) 
+   {
+      cout << "The number is positive." << endl;
+      cout << "Absolute value: " << number << endl;
+   }
+   else {
+      cout << "The number is zero." << endl;
+      cout << "Absolute value: 0" << endl;
+   }
+
+   return 0;
+}  
