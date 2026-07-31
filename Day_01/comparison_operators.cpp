72e37354f35a491c4871200fe65db51e19ab3499
bharath <myasuslaptop45@gmail.com>
2026-07-31 12:38:01 +0530

    Create comparison_operators.cpp

---
+++ Day_01/comparison_operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   int a = 8;
+   int b = 5;
+
+   cout << boolalpha;
+   cout << "a > b: " << (a > b) << endl;
+   cout << "a < b: " << (a < b) << endl;
+   cout << "a <= b: " << (a <= b) << endl;
+   cout << "a >= b: " << (a >= b) << endl;
+   cout << "a == b: " << (a == b) << endl;
+   cout << "a != b: " << (a != b) << endl;
+   cout << a << endl;
+   
+   return 0;
+}  
