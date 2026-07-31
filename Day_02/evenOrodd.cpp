285931b6dd4943a783c07c9e133c9b4c0cd2d59a
bharath <myasuslaptop45@gmail.com>
2026-07-31 14:03:46 +0530

    Create evenOrodd.cpp

---
+++ Day_02/evenOrodd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isEvent(int num)
+{
+    if(num%2==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if(isEvent(num))
+    {
+        cout << num << " is even." << endl;
+    }
+    else
+    {
+        cout << num << " is odd." << endl;
+    }
+    return 0;
+}   
