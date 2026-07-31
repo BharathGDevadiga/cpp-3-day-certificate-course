d9732efcc9a856aceaf04d93b1cab33e82fbeda7
bharath <myasuslaptop45@gmail.com>
2026-07-31 22:00:40 +0530

    Create factorial_recursion.cpp

---
+++ Day_02/factorial_recursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int number){
+	if(number == 0 ){
+		return 1;
+	}
+
+	return number * factorial(number - 1);
+}
+
+int main(){
+	factorial(5);
+
+	cout << "Factorial: " << factorial(5) << endl;
+	return 0;
+}
