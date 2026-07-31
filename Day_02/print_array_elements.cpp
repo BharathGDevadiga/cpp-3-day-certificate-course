49cff29f6b3db71abbab11ba007fbe9d0aa001d5
bharath <myasuslaptop45@gmail.com>
2026-07-31 12:52:42 +0530

    Create print_array_elements.cpp

---
+++ Day_02/print_array_elements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int numbers[] = {10, 20, 30, 40, 50};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    for (int i = 0; i < size; i++)
+      {
+        cout << numbers[i] << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
