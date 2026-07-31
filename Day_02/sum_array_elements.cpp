460bc602e6530096300c466ad491ed0208035cb7
bharath <myasuslaptop45@gmail.com>
2026-07-31 12:56:59 +0530

    Create sum_array_elements.cpp

---
+++ Day_02/sum_array_elements.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin>>n;
+
+    int arr[n];     
+
+    cout << "Enter array values: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int sum = 0;
+    for(int i=0; i<n; i++)
+    {
+        sum += arr[i];
+    }
+
+    cout << "Sum of array elements: " << sum;
+    return 0;
+}
