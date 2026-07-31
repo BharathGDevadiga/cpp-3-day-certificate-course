7ace03ebf45e7ed73e3c2dd22b0f9941dc076983
bharath <myasuslaptop45@gmail.com>
2026-07-31 13:58:13 +0530

    Create count_vowels.cpp

---
+++ Day_02/count_vowels.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "hello";
+
+    int vowelCount = 0;
+    for(int i=0; i<str.length(); i++)
+    {
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+            vowelCount++;
+        }
+    }
+
+    cout << "Number of vowels: " << vowelCount;
+    return 0;
+}
