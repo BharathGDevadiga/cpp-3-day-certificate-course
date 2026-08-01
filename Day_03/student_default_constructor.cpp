d7c4b384660a146916827a0b7b6ae61f5713292f
bharath <myasuslaptop45@gmail.com>
2026-08-01 12:18:28 +0530

    Create student_default_constructor.cpp

---
+++ Day_03/student_default_constructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+public:
+    string name;
+    int age;
+    student()
+    {
+        name="Bharath";
+        age=22;
+    }
+    void display()
+    {
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age : "<<age<<endl;
+    }
+};
+int main()
+{
+    student s1;
+    s1.display();
+    return 0;
+}
