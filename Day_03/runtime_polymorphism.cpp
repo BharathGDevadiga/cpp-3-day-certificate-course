dcc6e56afdef8f8a259aedf39949388e628c2771
bharath <myasuslaptop45@gmail.com>
2026-08-01 12:23:48 +0530

    Create runtime_polymorphism.cpp

---
+++ Day_03/runtime_polymorphism.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void sound()
+    {
+        cout<<"Animal sound"<<endl;
+    }
+};
+class dog:public Animal
+{
+public:
+    void sound()
+    {
+        cout<<"Dog barks"<<endl;
+    }
+};
+int main()
+{
+    Animal *a1;
+    dog d1;
+    a1=&d1; 
+    a1->sound();
+    return 0;
+}
