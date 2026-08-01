3ffe6a6dfff3ca94c7383bfc8d90d09a18589c40
bharath <myasuslaptop45@gmail.com>
2026-08-01 11:29:05 +0530

    Update class_and_object.cpp

--- Day_03/class_and_object.cpp
+++ Day_03/class_and_object.cpp
@@ -1,17 +1,20 @@
 #include<iostream>
 using namespace std;
 
-class student{
+class student
+{
 public:
     string name;
     int age;
 
-    void display(){
+    void display()
+{
         cout<<"Name : "<<name<<endl;
         cout<<"Age : "<<age<<endl;
 }
 };
-int main(){
+int main()
+{
     student s1;
     s1.name="Bharath";
     s1.age=21;
