fbd369a2e1021ecfb7dae0bf533a10f43741896f
bharath <myasuslaptop45@gmail.com>
2026-08-01 12:25:17 +0530

    Create inheritance_animal_dog.cpp

---
+++ Day_03/inheritance_animal_dog.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+//parent class
+class Animal
+{
+public:
+    string name;
+   
+    void eat()
+    {
+        cout<<"Animal is eating"<<endl;
+    }
+   
+    };
+
+//child class
+class Dog:public Animal
+{
+public:
+    void bark()
+    {
+        cout<<"Dog is barking"<<endl;
+    }
+};
+
+int main()
+{
+    Dog d1;
+    d1.eat(); // Inherited from Animal
+    d1.bark(); // Specific to Dog
+    return 0;
+}
