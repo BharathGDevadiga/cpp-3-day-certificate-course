0ad083142d48fc2f21583370214613712b421ad6
bharath <myasuslaptop45@gmail.com>
2026-08-01 12:12:37 +0530

    Create pointer_size.cpp

---
+++ Day_03/pointer_size.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<sizeof(int)<<endl;   
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(double)<<endl;
+  
+    cout<<endl;
+  
+    cout<<sizeof(int*)<<endl;
+    cout<<sizeof(char*)<<endl;
+    cout<<sizeof(double*)<<endl;
+    return 0;
+}
