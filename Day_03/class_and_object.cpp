4935475668374e027c60c708e937f13e509db2bb
bharath <myasuslaptop45@gmail.com>
2026-08-01 11:27:38 +0530

    Create class_and_object.cpp

---
+++ Day_03/class_and_object.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class student{
+public:
+    string name;
+    int age;
+
+    void display(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age : "<<age<<endl;
+}
+};
+int main(){
+    student s1;
+    s1.name="Bharath";
+    s1.age=21;
+    s1.display();
+}
