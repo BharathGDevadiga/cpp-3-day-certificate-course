2dc024ae4cf552b361e85e5f7ebd3db5ff803b5f
bharath <myasuslaptop45@gmail.com>
2026-08-01 11:30:53 +0530

    Create default_constructor.cpp

---
+++ Day_03/default_constructor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+public:
+    student()
+{
+        cout<<"My name is Bharath";
+    }
+};
+int main()
+{
+    student s1;
+    return 0;
+}
