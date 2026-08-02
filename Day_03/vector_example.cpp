dcdc1193fc229f4d2b448cf35d591eb465152148
bharath <myasuslaptop45@gmail.com>
2026-08-02 08:54:00 +0530

    Create vector_example.cpp

---
+++ Day_03/vector_example.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+  
+    for(int x:v)
+        cout<<x<<endl;
+  
+    return 0;
+
+}
